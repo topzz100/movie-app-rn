cdd722ab39d903cd43d9b347213668e2a80918be
Tope Adenekan <adenekantope.com>
2025-11-09 21:34:02 +0100

    search

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,11 +1,79 @@
-import { View, Text, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { XMarkIcon } from 'react-native-heroicons/outline';
+import { useNavigation } from '@react-navigation/native';
 
+const dataValues = [
+  { id: 1, uri: 'https://picsum.photos/id/1018/800/600', name: 'lorem ipsum222 ksla;j' },
+  { id: 2, uri: 'https://picsum.photos/id/1015/800/600', name: 'lorem ipsum' },
+  { id: 3, uri: 'https://picsum.photos/id/1025/800/600', name: 'lorem ipsum' },
+  { id: 4, uri: 'https://picsum.photos/id/1035/800/600', name: 'lorem ipsum' },
+  { id: 5, uri: 'https://picsum.photos/id/1043/800/600', name: 'lorem ipsum' },
+];
 const { width, height } = Dimensions.get('window');
 export default function SearchScreen() {
+  const navigation = useNavigation<any>();
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-neutral-800">
+      <View className="mx-4 mb-3 flex-row items-center justify-between rounded-full border border-neutral-500">
+        <TextInput
+          placeholder="Search Movie"
+          placeholderTextColor={'lightgray'}
+          className="h-full flex-1 pl-6 text-base font-semibold tracking-wider text-white"
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          className="m-1 rounded-full bg-neutral-500 p-3">
+          <XMarkIcon size="25" color={'white'} />
+        </TouchableOpacity>
+      </View>
+      {/* Results */}
+      {dataValues.length < 0 ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ marginHorizontal: 15 }}
+          className="gap-3">
+          <Text className="ml-1 font-semibold text-white">Results ({dataValues.length})</Text>
+          <View className="mt-4 flex-row flex-wrap justify-between">
+            {dataValues.map((item, index) => {
+              return (
+                <TouchableWithoutFeedback
+                  key={index}
+                  onPress={() => navigation.navigate('Movie', item)}>
+                  <View className="mb-4 space-y-2">
+                    <Image
+                      className="rounded-3xl"
+                      source={{ uri: item.uri }}
+                      style={{ width: width * 0.44, height: height * 0.33 }}
+                    />
+                    <Text className="ml-1 text-neutral-300">{item.name}</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            })}
+          </View>
+        </ScrollView>
+      ) : (
+        <View className=" flex-1  items-center ">
+          {/* <Image
+            source={{
+              uri: 'https://www.freepik.com/free-vector/hand-drawn-no-data-illustration_59563752.htm#fromView=keyword&page=2&position=4&uuid=3d30c427-d7b8-4819-83e9-a5377a98059e&query=Empty+data',
+            }}
+            className="h-40 w-40"
+          /> */}
+          <Text className="mt-[100] items-center text-white"> No data available</Text>
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
